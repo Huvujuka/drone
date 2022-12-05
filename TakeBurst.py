dff941c08b05f2b0a0fc47698e8979c6b4b63a6e
Huvujuka <115858403+Huvujuka@users.noreply.github.com>
2022-12-04 19:24:37 -0600

    Update TakeBurst.py

--- TakeBurst.py
+++ TakeBurst.py
@@ -6,7 +6,7 @@ import time
 import multiprocessing
 
 def takeBurst(Burst_Num):
-    startTime = time.time()
+
     Img_Array = []
     currImg = cv2.VideoCapture(0)                                         #Initialize camera
 
@@ -26,8 +26,7 @@ def takeBurst(Burst_Num):
 
     for i in range(len(Img_Array)):
         cv2.imwrite("/home/odroid/Desktop/Demo/CompletedImages/Burst/Img" + str(i) + ".jpg", Img_Array[i])
-    runTime = time.time() - startTime
-    print("Took " + str(runTime))
+
 
 
 num_Imgs = int(input("Number of pictures to take: "))
